59c2fc924515a57c153a3cc8a95f052e21ec4e88
abala2017 <abala2017@gmail.com>
2020-03-15 15:22:32 -0500

    ex6 finished

--- ml-ex6/ex6/dataset3Params.m
+++ ml-ex6/ex6/dataset3Params.m
@@ -23,9 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+params = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+minC = 0.01;
+minSig = 0.01;
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+predictions = svmPredict(model, Xval);
+minCost = mean(double(predictions ~= yval));
 
+for i = 1:8
+  for j = 1:8
+    model= svmTrain(X, y, params(i), @(x1, x2) gaussianKernel(x1, x2, params(j)));
+    predictions = svmPredict(model, Xval);
+    cost = mean(double(predictions ~= yval));
+    
+    if cost < minCost,
+      minC = params(i);
+      minSig = params(j);
+      minCost = cost;
+    endif
+    
+  endfor
+endfor
 
-
+C = minC;
+sigma = minSig;
 
 
 
