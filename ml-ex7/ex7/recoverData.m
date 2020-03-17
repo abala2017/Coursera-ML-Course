952d48225850bc8925ae5fa357aa869526b4e7a1
abala2017 <abala2017@gmail.com>
2020-03-16 23:04:31 -0500

    assignment 7 completed

--- ml-ex7/ex7/recoverData.m
+++ ml-ex7/ex7/recoverData.m
@@ -20,7 +20,15 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %
 %               Notice that U(j, 1:K) is a row vector.
 %               
-
+%Ured = U(:,1:K);
+%X_rec = Ured * Z';
+%X_rec = X_rec';
+for i = 1:size(Z)(1)
+  for j = 1:size(U)(1)
+    v = Z(i,:)';
+    X_rec(i,j) = v' * U(j, 1:K)';
+  endfor
+endfor
 
 
 % =============================================================
